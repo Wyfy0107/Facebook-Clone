c20162c54af6c866ad2b7c726719fbde52b0ce07
Duy Nguyen <nguyenduy010798@gmail.com>
2020-08-09 15:39:24 +0300

    commit build folder

---
+++ client/build/precache-manifest.d804aa5955e3289386880e88258b9b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4eb0747a5e7ef569c12cc72964814ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20f3c7ecd2a84be63d03",
+    "url": "/static/css/main.d4027325.chunk.css"
+  },
+  {
+    "revision": "f7445bd491d80ab20d4b",
+    "url": "/static/js/2.73ca085d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.73ca085d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f3c7ecd2a84be63d03",
+    "url": "/static/js/main.774d3a1e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
