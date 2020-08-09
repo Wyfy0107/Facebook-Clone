d67a3a47d90c383ff2ad5ba6fd32ccd3b1ced581
Duy Nguyen <nguyenduy010798@gmail.com>
2020-08-09 15:31:57 +0300

    test

--- server.js
+++ server.js
@@ -39,11 +39,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
-app.use(express.static(path.join(__dirname, "client/build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 
 //routes
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/server/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 //register
